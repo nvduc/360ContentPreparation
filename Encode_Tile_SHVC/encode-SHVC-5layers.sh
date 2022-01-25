d2a9d4a3692b7ecf5e69602fa62f9c777e2df866
nvduc <nvduc712@gmail.com>
2022-01-25 11:28:38 +0900

    update encode script

--- Encode_Tile_SHVC/encode-SHVC-5layers.sh
+++ Encode_Tile_SHVC/encode-SHVC-5layers.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-path="../Dataset/RollerCoaster/6f_2x2/tile_yuv/"
+vname="RollerCoaster"
+path="../Dataset/${vname}/6f_2x2/tile_yuv/"
 W="480"
 H="480"
 QP0="38"
@@ -11,6 +12,6 @@ QP4="20"
 for (( i = 0; i < 6; i++ )); do
 	for (( j = 0; j < 4; j++ )); do
 		tile_name="f${i}_t${j}_480x480"
-		../bin/TAppEncoderSHVC -c encoder_randomaccess_main.cfg -c 5layers.cfg -c RollerCoaster-SNR-5layers.cfg -q0 ${QP0}  -i0 ${path}${tile_name}.yuv -q1 ${QP1} -i1 ${path}${tile_name}.yuv -q2 ${QP2} -i2 ${path}${tile_name}.yuv -q3 ${QP3} -i3 ${path}${tile_name}.yuv -q4 ${QP4} -i4 ${path}${tile_name}.yuv -b ${tile_name}_SHVC_5layers.bin  > log_encode_${tile_name}_SHVC_5layers.txt
+		../bin/TAppEncoderSHVC -c encoder_randomaccess_main.cfg -c 5layers.cfg -c RollerCoaster-SNR-5layers.cfg -q0 ${QP0}  -i0 ${path}${tile_name}.yuv -q1 ${QP1} -i1 ${path}${tile_name}.yuv -q2 ${QP2} -i2 ${path}${tile_name}.yuv -q3 ${QP3} -i3 ${path}${tile_name}.yuv -q4 ${QP4} -i4 ${path}${tile_name}.yuv -b ${vname}_${tile_name}_SHVC_5layers.bin  > log_encode_${vname}_${tile_name}_SHVC_5layers.txt
 	done
 done
